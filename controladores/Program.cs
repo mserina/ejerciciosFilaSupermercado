1da0d36b1ebf696792ca2b223d1b0d642e4ffb27
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-04-23 13:38:49 +0200

    Agregar archivos de proyecto.

---
+++ controladores/Program.cs
@@ -0,0 +1,65 @@
+﻿using ejerciciosCollection.servicios;
+
+namespace ejerciciosCollection.controladores
+{
+    /// <summary>
+    /// Clase principal del programa
+    /// msm -22042024
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Metodo que contiene el procedimiento principal
+        /// msm - 22042024
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            MenuInterfaz mi = new MenuImplementacion();
+            int opcionSelecionada;
+            bool cerrarMenu = false;
+           
+            Queue<string> clientes = new Queue<string>();
+
+
+            while (!cerrarMenu)
+            {
+                opcionSelecionada = mi.respuestaUsuario();
+
+                switch (opcionSelecionada)
+                {
+                    case 0:
+                        
+                        cerrarMenu = true;
+                        break;
+
+                    case 1:
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Inserte clientes");
+                        string respuestaInsertar = Console.ReadLine();
+                        clientes.Enqueue(respuestaInsertar);
+                        break;
+                    case 2:
+                        Console.WriteLine(" ");
+                        clientes.Dequeue();
+                        break;
+                    case 3:
+                        foreach (string cliente in clientes)
+                        {
+                            Console.WriteLine(" ");
+                            Console.WriteLine(cliente);
+                            Console.WriteLine(" ");
+                        }
+                        break;
+
+
+                }
+
+            }
+
+        }
+
+    }
+
+        //UTILIZARIA HASH SET POR QUE NOS INDICA SI EXISTE UN ELEMENTO ESPEFICO EN LA LISTA
+    }
---
+++ servicios/MenuImplementacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciosCollection.servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int respuestaUsuario()
+        {
+            int opcionIntroducida;
+            Console.WriteLine("######################################");
+            Console.WriteLine("0. Cerrar aplicacion");
+            Console.WriteLine("1. Introducir cliente");
+            Console.WriteLine("2. Quitar cliente");
+            Console.WriteLine("3. Ver cola");
+
+            opcionIntroducida = Console.ReadKey(true).KeyChar - ('0');
+
+            return opcionIntroducida;
+        }
+    }
+}
